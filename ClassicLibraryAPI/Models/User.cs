d7dbd70b2792c6116bb5d39e577770a5a8477c31
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-02-17 15:44:41 -0300

    Criando classe de modelo para o usuario

---
+++ ClassicLibraryAPI/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace ClassicLibraryAPI.Models {
+    public class User {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adress { get; set; }
+
+        public User() {
+            if (FirstName == null) {
+                FirstName = "";
+            }
+            if (LastName == null) {
+                LastName = "";
+            }
+            if (Email == null) {
+                Email = "";
+            }
+            if (PhoneNumber == null) {
+                PhoneNumber = "";
+            }
+            if (Adress == null) {
+                Adress = "";
+            }
+        }
+    }
+}
