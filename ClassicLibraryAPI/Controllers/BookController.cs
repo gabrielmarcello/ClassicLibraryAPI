9e25e92e6eeb711e77fec82ae78351fb5d77b10a
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-02-28 15:22:47 -0300

    Criando controller de livros e endpoint get

---
+++ ClassicLibraryAPI/Controllers/BookController.cs
@@ -0,0 +1,41 @@
+﻿using ClassicLibraryAPI.Data;
+using ClassicLibraryAPI.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace ClassicLibraryAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController {
+
+        private readonly DataContextDapper _dapper;
+
+        public BookController(IConfiguration config) {
+            _dapper = new DataContextDapper(config);
+        }
+
+        [HttpGet("Books/{bookId}")]
+        public IEnumerable<Book> GetBooks(int bookId) {
+            string sql = "EXEC ClassicLibrarySchema.spBooks_Get";
+
+            string stringParameters = "";
+
+            DynamicParameters sqlParameters = new DynamicParameters();
+
+            if (bookId > 0) {
+                stringParameters += ", @LivroId = @LivroIdParameter";
+                sqlParameters.Add("@LivroIdParameter", bookId, DbType.Int32);
+            }
+
+            if (stringParameters.Length > 0) {
+                sql += stringParameters.Substring(1);
+            }
+
+            IEnumerable<Book> books = _dapper.LoadDataWithParameters<Book>(sql, sqlParameters);
+            return books;
+
+        }
+
+    }
+}
--- ClassicLibraryAPI/Models/Book.cs
+++ ClassicLibraryAPI/Models/Book.cs
@@ -1,20 +1,20 @@
 ﻿namespace ClassicLibraryAPI.Models {
     public class Book {
-        public int Id { get; set; }
-        public float Price { get; set; }
-        public string Title { get; set; }
-        public string Category { get; set; }
-        public string Idiom { get; set; }
-        public string Autor { get; set; }
-        public string Type { get; set; }
-        public int PagesNumber { get; set; }
-        public string Publisher { get; set; }
-        public DateOnly PublishDate { get; set; }
-        public string Dimentions { get; set; }
-        public string Image { get; set; }
-        public int StockAmount { get; set; }
-        public bool Availability { get; set; }
-        public string Condition { get; set; }
+        public int LivroId { get; set; }
+        public float Preco { get; set; }
+        public string Titulo { get; set; }
+        public string Idioma { get; set; }
+        public string NomeCategoria { get; set; }
+        public string NomeAutor { get; set; }
+        public string Tipo { get; set; }
+        public int Numero_Paginas { get; set; }
+        public string Editora { get; set; }
+        public DateOnly DataPublicacao { get; set; }
+        public string Dimensao { get; set; }
+        public string Imagem { get; set; }
+        public int Quantidade_Estoque { get; set; }
+        public bool Disponibilidade { get; set; }
+        public string Condicao { get; set; }
 
     }
 }
