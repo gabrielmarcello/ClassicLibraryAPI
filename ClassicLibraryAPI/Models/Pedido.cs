d5a579590abfeb17e4a936ecb3bb607442ed36d6
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-03-05 11:56:32 -0300

    Criando classe de Pedido

---
+++ ClassicLibraryAPI/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassicLibraryAPI.Models {
+    public class Pedido {
+        public int Id { get; set; }
+        public User Cliente { get; set; }
+        public List<ItemCarrinho> Itens { get; set; }
+        public decimal Total { get; set; }
+        public string StatusPagamento { get; set; }
+
+        public Pedido(int id, User cliente, List<ItemCarrinho> itens, decimal total) {
+            Id = id;
+            Cliente = cliente;
+            Itens = itens;
+            Total = total;
+            StatusPagamento = "Pendente";
+        }
+    }
+}
