e374c9d3e2623efc509024250cfa56c9bc0ec46c
gabrielmarcello <gabriel.marcellocf@gmail.com>
2025-03-05 11:50:48 -0300

    Criando carrinho de livros

---
+++ ClassicLibraryAPI/Models/Carrinho.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassicLibraryAPI.Models {
+    public class Carrinho {
+        public List<ItemCarrinho> Itens { get; set; }
+
+        public Carrinho() {
+            Itens = new List<ItemCarrinho>();
+        }
+
+        public void AdicionarItem(Book livro, int quantidade) {
+            var item = Itens.FirstOrDefault(i => i.Livro.LivroId == livro.LivroId);
+            if (item == null) {
+                Itens.Add(new ItemCarrinho(livro, quantidade));
+            }
+            else {
+                item.Quantidade += quantidade;
+            }
+        }
+
+        public decimal CalcularTotal() {
+            return (decimal)Itens.Sum(i => i.Livro.Preco * i.Quantidade);
+        }
+    }
+}
--- ClassicLibraryAPI/Models/ItemCarrinho.cs
+++ ClassicLibraryAPI/Models/ItemCarrinho.cs
@@ -2,5 +2,10 @@
     public class ItemCarrinho {
         public Book Livro { get; set; }
         public int Quantidade { get; set; }
+
+        public ItemCarrinho(Book livro, int quantidade) {
+            Livro = livro;
+            Quantidade = quantidade;
+        }
     }
 }
